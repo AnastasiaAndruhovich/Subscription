e2889ca231f4f8869443e5dcbecc2b13187502c0
AnastasiaAndruhovich550503 <anastasia.andruhovich@gmail.com>
2018-01-04 17:34:02 +0300

    Create DAOTechnicalException

---
+++ src/main/java/by/andruhovich/subscription/exception/DAOTechnicalException.java
@@ -0,0 +1,22 @@
+package by.andruhovich.subscription.exception;
+
+public class DAOTechnicalException extends Exception{
+    public DAOTechnicalException() {
+    }
+
+    public DAOTechnicalException(String message) {
+        super(message);
+    }
+
+    public DAOTechnicalException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public DAOTechnicalException(Throwable cause) {
+        super(cause);
+    }
+
+    public DAOTechnicalException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
