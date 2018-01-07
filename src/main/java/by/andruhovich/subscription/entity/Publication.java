d7c97cc940cbf85302e81b9fc52245e031b90880
AnastasiaAndruhovich550503 <anastasia.andruhovich@gmail.com>
2018-01-07 21:51:01 +0300

    Add new fields in Publication class. Create constructor

--- src/main/java/by/andruhovich/subscription/entity/Publication.java
+++ src/main/java/by/andruhovich/subscription/entity/Publication.java
@@ -1,12 +1,32 @@
 package by.andruhovich.subscription.entity;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
 public class Publication {
     private int publicationId;
     private String name;
+    private int publicationTypeId;
+    private int genreId;
     private String description;
     private BigDecimal price;
+
+    public Publication(String name, int publicationTypeId, int genreId, String description, BigDecimal price) {
+        this.name = name;
+        this.publicationTypeId = publicationTypeId;
+        this.genreId = genreId;
+        this.description = description;
+        this.price = price;
+    }
+
+    public Publication(int publicationId, String name, int publicationTypeId, int genreId, String description, BigDecimal price) {
+        this.publicationId = publicationId;
+        this.name = name;
+        this.publicationTypeId = publicationTypeId;
+        this.genreId = genreId;
+        this.description = description;
+        this.price = price;
+    }
 
     public int getPublicationId() {
         return publicationId;
@@ -22,6 +42,22 @@ public class Publication {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public int getPublicationTypeId() {
+        return publicationTypeId;
+    }
+
+    public void setPublicationTypeId(int publicationTypeId) {
+        this.publicationTypeId = publicationTypeId;
+    }
+
+    public int getGenreId() {
+        return genreId;
+    }
+
+    public void setGenreId(int genreId) {
+        this.genreId = genreId;
     }
 
     public String getDescription() {
@@ -43,24 +79,30 @@ public class Publication {
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
+        if (!(o instanceof Publication)) return false;
         Publication that = (Publication) o;
-
-        if (publicationId != that.publicationId) return false;
-        if (name != null ? !name.equals(that.name) : that.name != null) return false;
-        if (description != null ? !description.equals(that.description) : that.description != null) return false;
-        if (price != null ? !price.equals(that.price) : that.price != null) return false;
-
-        return true;
+        return publicationId == that.publicationId &&
+                publicationTypeId == that.publicationTypeId &&
+                genreId == that.genreId &&
+                Objects.equals(name, that.name) &&
+                Objects.equals(description, that.description) &&
+                Objects.equals(price, that.price);
     }
 
     @Override
     public int hashCode() {
-        int result = publicationId;
-        result = 31 * result + (name != null ? name.hashCode() : 0);
-        result = 31 * result + (description != null ? description.hashCode() : 0);
-        result = 31 * result + (price != null ? price.hashCode() : 0);
-        return result;
+        return Objects.hash(publicationId, name, publicationTypeId, genreId, description, price);
+    }
+
+    @Override
+    public String toString() {
+        return "Publication{" +
+                "publicationId=" + publicationId +
+                ", name='" + name + '\'' +
+                ", publicationTypeId=" + publicationTypeId +
+                ", genreId=" + genreId +
+                ", description='" + description + '\'' +
+                ", price=" + price +
+                '}';
     }
 }
