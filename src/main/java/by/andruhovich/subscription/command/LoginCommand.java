9b725051a3752fd01f1159f2c58e4f32b15c203b
AnastasiaAndruhovich550503 <anastasia.andruhovich@gmail.com>
2018-01-07 22:02:01 +0300

    Add block catch ServiceTechnicalException

--- src/main/java/by/andruhovich/subscription/command/LoginCommand.java
+++ src/main/java/by/andruhovich/subscription/command/LoginCommand.java
@@ -1,28 +1,33 @@
 package by.andruhovich.subscription.command;
 
+import by.andruhovich.subscription.exception.ServiceTechnicalException;
 import by.andruhovich.subscription.manager.ConfigurationManager;
 import by.andruhovich.subscription.manager.MessageManager;
-import by.andruhovich.subscription.receiver.UserReceiver;
+import by.andruhovich.subscription.service.UserService;
 
 import javax.servlet.http.HttpServletRequest;
 
 public class LoginCommand implements BaseCommand{
-    private UserReceiver userReceiver = new UserReceiver();
+    private UserService userService = new UserService();
     private static final String LOGIN = "login";
     private static final String PASSWORD = "password";
 
     public String execute(HttpServletRequest request) {
         String page;
-        //бизнес-логика? сделать методы static?
         String login = request.getParameter(LOGIN);
         String password = request.getParameter(PASSWORD);
 
-        if (userReceiver.checkLogin(login, password)) {
-            request.setAttribute("user", login);
-            page = ConfigurationManager.getProperty("path.page.main");
-        } else {
-            request.setAttribute("errorLoginPassMessage", MessageManager.getProperty("message.error"));
-            page = ConfigurationManager.getProperty("path.page.login");
+        try {
+            if (userService.checkLogin(login, password)) {
+                request.setAttribute("user", login);
+                page = ConfigurationManager.getProperty("path.page.main");
+            } else {
+                request.setAttribute("errorLoginPassMessage", MessageManager.getProperty("message.error"));
+                page = ConfigurationManager.getProperty("path.page.login");
+            }
+        } catch (ServiceTechnicalException e) {
+            //log
+            page = ConfigurationManager.getProperty("path.page.error");
         }
         return page;
     }
