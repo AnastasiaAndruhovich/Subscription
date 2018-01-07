9dbf31bac04df37ce760b25b7b389b83bbf6c30a
AnastasiaAndruhovich550503 <anastasia.andruhovich@gmail.com>
2018-01-07 21:56:10 +0300

    Create ServiceTechnicalException

---
+++ src/main/java/by/andruhovich/subscription/exception/ServiceTechnicalException.java
@@ -0,0 +1,22 @@
+package by.andruhovich.subscription.exception;
+
+public class ServiceTechnicalException extends Exception{
+    public ServiceTechnicalException() {
+    }
+
+    public ServiceTechnicalException(String message) {
+        super(message);
+    }
+
+    public ServiceTechnicalException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ServiceTechnicalException(Throwable cause) {
+        super(cause);
+    }
+
+    public ServiceTechnicalException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
