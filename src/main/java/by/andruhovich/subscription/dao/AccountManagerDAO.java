689e6a0479ebd00cdd137dbafab7dcf84f69e7fd
AnastasiaAndruhovich550503 <anastasia.andruhovich@gmail.com>
2018-01-08 11:10:18 +0300

    Create AccountManagerDAO

--- src/main/java/by/andruhovich/subscription/dao/AccountManagerDAO.java
+++ src/main/java/by/andruhovich/subscription/dao/AccountManagerDAO.java
@@ -5,8 +5,5 @@ import by.andruhovich.subscription.entity.Account;
 import java.sql.Connection;
 
 public abstract class AccountManagerDAO extends BaseDAO<Account>{
-    public AccountManagerDAO(Connection connection) {
-        super(connection);
-    }
 
 }
