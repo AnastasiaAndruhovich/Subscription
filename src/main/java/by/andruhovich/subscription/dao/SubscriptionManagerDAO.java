4830ee0426c85cd502017b467946c65ecc15cba1
AnastasiaAndruhovich550503 <anastasia.andruhovich@gmail.com>
2018-01-08 11:26:23 +0300

    Create SubscriptionManagerDAO

---
+++ src/main/java/by/andruhovich/subscription/dao/SubscriptionManagerDAO.java
@@ -0,0 +1,6 @@
+package by.andruhovich.subscription.dao;
+
+import by.andruhovich.subscription.entity.Subscription;
+
+public abstract class SubscriptionManagerDAO extends BaseDAO<Subscription> {
+}
