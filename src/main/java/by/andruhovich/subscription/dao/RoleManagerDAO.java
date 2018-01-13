0891ee6d07d4612855bb4b1c389c32f4b13595de
AnastasiaAndruhovich550503 <anastasia.andruhovich@gmail.com>
2018-01-13 17:16:15 +0300

    Create class RoleManagerDAO

---
+++ src/main/java/by/andruhovich/subscription/dao/RoleManagerDAO.java
@@ -0,0 +1,14 @@
+package by.andruhovich.subscription.dao;
+
+import by.andruhovich.subscription.entity.Role;
+import by.andruhovich.subscription.exception.DAOTechnicalException;
+
+import java.sql.Connection;
+
+public abstract class RoleManagerDAO extends BaseDAO<Role> {
+    public RoleManagerDAO(Connection connection) {
+        super(connection);
+    }
+
+    public abstract int findIdByName(String role) throws DAOTechnicalException;
+}
