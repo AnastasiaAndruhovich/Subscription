c0e81d348338ffc85127bbba3f8cb401e0e890aa
AnastasiaAndruhovich550503 <anastasia.andruhovich@gmail.com>
2018-01-13 17:18:40 +0300

    Rename var credit to loan

--- src/main/java/by/andruhovich/subscription/entity/Account.java
+++ src/main/java/by/andruhovich/subscription/entity/Account.java
@@ -1,21 +1,22 @@
 package by.andruhovich.subscription.entity;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
 public class Account {
     private int accountNumber;
     private BigDecimal balance;
-    private BigDecimal credit;
+    private BigDecimal loan;
 
-    public Account(BigDecimal balance, BigDecimal credit) {
+    public Account(BigDecimal balance, BigDecimal loan) {
         this.balance = balance;
-        this.credit = credit;
+        this.loan = loan;
     }
 
-    public Account(int accountNumber, BigDecimal balance, BigDecimal credit) {
+    public Account(int accountNumber, BigDecimal balance, BigDecimal loan) {
         this.accountNumber = accountNumber;
         this.balance = balance;
-        this.credit = credit;
+        this.loan = loan;
     }
 
     public int getAccountNumber() {
@@ -34,33 +35,26 @@ public class Account {
         this.balance = balance;
     }
 
-    public BigDecimal getCredit() {
-        return credit;
+    public BigDecimal getLoan() {
+        return loan;
     }
 
-    public void setCredit(BigDecimal credit) {
-        this.credit = credit;
+    public void setLoan(BigDecimal loan) {
+        this.loan = loan;
     }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
+        if (!(o instanceof Account)) return false;
         Account account = (Account) o;
-
-        if (accountNumber != account.accountNumber) return false;
-        if (balance != null ? !balance.equals(account.balance) : account.balance != null) return false;
-        if (credit != null ? !credit.equals(account.credit) : account.credit != null) return false;
-
-        return true;
+        return accountNumber == account.accountNumber &&
+                Objects.equals(balance, account.balance) &&
+                Objects.equals(loan, account.loan);
     }
 
     @Override
     public int hashCode() {
-        int result = accountNumber;
-        result = 31 * result + (balance != null ? balance.hashCode() : 0);
-        result = 31 * result + (credit != null ? credit.hashCode() : 0);
-        return result;
+        return Objects.hash(accountNumber, balance, loan);
     }
 }
