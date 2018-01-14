cbd598932b6f9688fa0edcb61deef5a5de25f99d
AnastasiaAndruhovich550503 <anastasia.andruhovich@gmail.com>
2018-01-14 16:06:54 +0300

    Create ManagerDAO

---
+++ src/main/java/by/andruhovich/subscription/dao/ManagerDAO.java
@@ -0,0 +1,18 @@
+package by.andruhovich.subscription.dao;
+
+import by.andruhovich.subscription.exception.DAOTechnicalException;
+
+import java.sql.Connection;
+import java.util.List;
+
+public abstract class ManagerDAO <T> extends BaseDAO{
+    public ManagerDAO(Connection connection) {
+        super(connection);
+    }
+
+    public abstract int create(T entity) throws DAOTechnicalException;
+    public abstract boolean delete(T entity) throws DAOTechnicalException;
+    public abstract T findEntityById(int id) throws DAOTechnicalException;
+    public abstract List<T> findAll() throws DAOTechnicalException;
+    public abstract boolean update(T entity) throws DAOTechnicalException;
+}
