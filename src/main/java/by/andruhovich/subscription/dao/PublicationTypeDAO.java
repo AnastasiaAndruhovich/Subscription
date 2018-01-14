cc55f28f6168c17d03a8b637b1c918e92c593b5b
AnastasiaAndruhovich550503 <anastasia.andruhovich@gmail.com>
2018-01-14 16:07:14 +0300

    Create PublicationTypeDAO

---
+++ src/main/java/by/andruhovich/subscription/dao/PublicationTypeDAO.java
@@ -0,0 +1,90 @@
+package by.andruhovich.subscription.dao;
+
+import by.andruhovich.subscription.dao.ManagerDAO;
+import by.andruhovich.subscription.entity.Publication;
+import by.andruhovich.subscription.entity.PublicationType;
+import by.andruhovich.subscription.exception.DAOTechnicalException;
+import by.andruhovich.subscription.mapper.PublicationTypeMapper;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.LinkedList;
+import java.util.List;
+
+public class PublicationTypeDAO extends ManagerDAO<PublicationType> {
+    private static final String INSERT_PUBLICATION_TYPE = "INSERT INTO publication_types(name) VALUE (?)";
+    private static final String SELECT_LAST_INSERT_ID = "SELECT LAST_INSERT_ID()";
+    private static final String DELETE_PUBLICATION_TYPE_BY_ID = "DELETE FROM publications WHERE publication_id = ?";
+    private static final String SELECT_ALL_PUBLICATION_TYPES = "SELECT * FROM publications";
+    private static final String UPDATE_PUBLICATION_TYPE = "UPDATE publications SET publication_type_id = ?, name = ?";
+
+    public PublicationTypeDAO(Connection connection) {
+        super(connection);
+    }
+
+    @Override
+    public int create(PublicationType entity) throws DAOTechnicalException {
+        PreparedStatement preparedStatement = null;
+        PublicationTypeMapper mapper = new PublicationTypeMapper();
+
+        try {
+            preparedStatement = connection.prepareStatement(INSERT_PUBLICATION_TYPE);
+            preparedStatement = mapper.mapEntityToPreparedStatement(preparedStatement, entity);
+            preparedStatement.executeQuery();
+            preparedStatement = connection.prepareStatement(SELECT_LAST_INSERT_ID);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            int id = resultSet.getInt("publication_type_id");
+            return id;
+        } catch (SQLException e) {
+            throw new DAOTechnicalException(e.getMessage());
+        } finally {
+            close(preparedStatement);
+        }
+    }
+
+    @Override
+    public boolean delete(PublicationType entity) throws DAOTechnicalException {
+        PreparedStatement preparedStatement = null;
+
+        try {
+            preparedStatement = connection.prepareStatement(DELETE_PUBLICATION_TYPE_BY_ID);
+            preparedStatement.setInt(1, entity.getPublicationTypeId());
+            preparedStatement.executeQuery();
+            return true;
+        } catch (SQLException e) {
+            throw new DAOTechnicalException(e.getMessage());
+        } finally {
+            close(preparedStatement);
+        }
+    }
+
+    @Override
+    public PublicationType findEntityById(int id) throws DAOTechnicalException {
+        return null;
+    }
+
+    @Override
+    public List<PublicationType> findAll() throws DAOTechnicalException {
+        return null;
+    }
+
+    @Override
+    public boolean update(PublicationType entity) throws DAOTechnicalException {
+        PreparedStatement preparedStatement = null;
+
+        try {
+            preparedStatement = connection.prepareStatement(UPDATE_PUBLICATION_TYPE);
+            PublicationTypeMapper mapper = new PublicationTypeMapper();
+            preparedStatement = mapper.mapEntityToPreparedStatement(preparedStatement, entity);
+            preparedStatement.executeQuery();
+            return true;
+        } catch (SQLException e) {
+            throw new DAOTechnicalException(e.getMessage());
+        } finally {
+            close(preparedStatement);
+        }
+    }
+
+}
