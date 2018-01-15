523337f7b22244265bd7a4112e7a1432aebfc218
AnastasiaAndruhovich550503 <anastasia.andruhovich@gmail.com>
2018-01-15 15:51:38 +0300

    Rename ResourceTechnicalException into ConnectionTechnicalException

--- src/main/java/by/andruhovich/subscription/dao/DAOFactory.java
+++ src/main/java/by/andruhovich/subscription/dao/DAOFactory.java
@@ -1,7 +1,7 @@
 package by.andruhovich.subscription.dao;
 
 import by.andruhovich.subscription.dao.impl.*;
-import by.andruhovich.subscription.exception.ResourceTechnicalException;
+import by.andruhovich.subscription.exception.ConnectionTechnicalException;
 import by.andruhovich.subscription.pool.ConnectionPool;
 
 import java.sql.Connection;
@@ -22,39 +22,41 @@ public class DAOFactory {
         return instance;
     }
 
-    private Connection getConnection() throws ResourceTechnicalException {
+    private Connection getConnection() throws ConnectionTechnicalException {
         Connection connection = connectionPool.getConnection(WAITING_TIME);
         if (connection != null) {
             return connection;
         }
-        else throw new ResourceTechnicalException("Can't get connection with database!");
+        else {
+            throw new ConnectionTechnicalException("Can't get connection with database!");
+        }
     }
 
-    public UserDAO createUserDAO() throws ResourceTechnicalException {
+    public UserDAO createUserDAO() throws ConnectionTechnicalException {
         return new UserDAO(getConnection());
     }
 
-    public AccountDAO createAccountDAO() throws ResourceTechnicalException {
+    public AccountDAO createAccountDAO() throws ConnectionTechnicalException {
         return new AccountDAO(getConnection());
     }
 
-    public RoleDAO createRoleDAO() throws ResourceTechnicalException {
+    public RoleDAO createRoleDAO() throws ConnectionTechnicalException {
         return new RoleDAO(getConnection());
     }
 
-    public GenreDAO createGenreDAO() throws ResourceTechnicalException {
+    public GenreDAO createGenreDAO() throws ConnectionTechnicalException {
         return new GenreDAO(getConnection());
     }
 
-    public PublicationDAO createPublicationDAO() throws ResourceTechnicalException {
+    public PublicationDAO createPublicationDAO() throws ConnectionTechnicalException {
         return new PublicationDAO(getConnection());
     }
 
-    public PublicationTypeDAO createPublicationTypeDAO() throws ResourceTechnicalException {
+    public PublicationTypeDAO createPublicationTypeDAO() throws ConnectionTechnicalException {
         return new PublicationTypeDAO(getConnection());
     }
 
-    public AuthorPublicationDAO createAuthorPublicationDAO() throws ResourceTechnicalException {
+    public AuthorPublicationDAO createAuthorPublicationDAO() throws ConnectionTechnicalException {
         return new AuthorPublicationDAO(getConnection());
     }
 }
