119b3989745d92e1afda0c1ce8bf8b38bc5db85b
AnastasiaAndruhovich550503 <anastasia.andruhovich@gmail.com>
2018-01-18 22:38:32 +0300

    Create ServiceValidator

---
+++ src/main/java/by/andruhovich/subscription/service/validator/ServiceValidator.java
@@ -0,0 +1,21 @@
+package by.andruhovich.subscription.service.validator;
+
+import java.util.regex.Pattern;
+
+public class ServiceValidator {
+    private static final String REGEX_POSTAL_INDEX_ = "\\d{6}";
+    private static final String REGEX_LOGIN = "\\b[a-zA-Z][a-zA-Z0-9\\-._]{3,30}\\b";
+    private static final String REGEX_MONEY_VALUE = "^[\\d]+?\\.[\\d]{2}$";
+
+    public static boolean verifyPostalIndex(String postalIndex) {
+        return Pattern.matches(REGEX_POSTAL_INDEX_, postalIndex);
+    }
+
+    public static boolean verifyLogin(String login) {
+        return Pattern.matches(REGEX_LOGIN, login);
+    }
+
+    public static boolean verifyMoneyValue(String value) {
+        return Pattern.matches(REGEX_MONEY_VALUE, value);
+    }
+}
