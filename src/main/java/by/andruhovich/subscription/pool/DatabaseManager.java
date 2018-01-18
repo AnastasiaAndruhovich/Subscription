988aa81c0b2d23b7b3596b84bfb93a63e272688d
AnastasiaAndruhovich550503 <anastasia.andruhovich@gmail.com>
2018-01-18 10:11:46 +0300

    Add catch MissingResourceException

--- src/main/java/by/andruhovich/subscription/pool/DatabaseManager.java
+++ src/main/java/by/andruhovich/subscription/pool/DatabaseManager.java
@@ -1,17 +1,24 @@
 package by.andruhovich.subscription.pool;
 
+import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
 class DatabaseManager {
     private static DatabaseManager instance;
     private static ResourceBundle resourceBundle;
+    private static final String DATABASE_CONFIG = "config/database";
 
     private DatabaseManager() {}
 
     static DatabaseManager getInstance() {
         if (instance == null) {
             instance = new DatabaseManager();
-            resourceBundle = ResourceBundle.getBundle("database");
+            try {
+                resourceBundle = ResourceBundle.getBundle(DATABASE_CONFIG);
+            } catch (MissingResourceException e) {
+                //TODO log
+                throw new RuntimeException("There is no database configuration file", e);
+            }
         }
         return instance;
     }
