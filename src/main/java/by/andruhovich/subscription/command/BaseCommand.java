503c7d14d170fdbfdbe011903a01911bcc12bd76
AnastasiaAndruhovich550503 <anastasia.andruhovich@gmail.com>
2018-01-18 22:37:39 +0300

    Refactor command

--- src/main/java/by/andruhovich/subscription/command/BaseCommand.java
+++ src/main/java/by/andruhovich/subscription/command/BaseCommand.java
@@ -1,7 +1,8 @@
 package by.andruhovich.subscription.command;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public interface BaseCommand {
-    String execute(HttpServletRequest request);
+    String execute(HttpServletRequest request, HttpServletResponse response);
 }
--- src/main/java/by/andruhovich/subscription/command/CommandFactory.java
+++ src/main/java/by/andruhovich/subscription/command/CommandFactory.java
@@ -1,7 +1,6 @@
 package by.andruhovich.subscription.command;
 
-import by.andruhovich.subscription.command.BaseCommand;
-import by.andruhovich.subscription.command.CommandMap;
+import by.andruhovich.subscription.command.user.UserCommandMap;
 import by.andruhovich.subscription.manager.MessageManager;
 
 import javax.servlet.http.HttpServletRequest;
@@ -9,19 +8,22 @@ import javax.servlet.http.HttpServletRequest;
 public class CommandFactory {
     public BaseCommand defineCommand(HttpServletRequest request) {
         BaseCommand command;
-        // извлечение имени команды из запроса
+        final String WRONG_ACTION = "wrongAction";
+        final String WRONG_ACTION_MESSAGE = "message.wrongaction";
+
         String action = request.getParameter("command");
         if (action == null || action.isEmpty()) {
-            // если команда не задана в текущем запросе emptyCommand???
             return null;
         }
-        // получение объекта, соответствующего команде
+
         try {
-            CommandMap commandMap = CommandMap.getInstance();
-            command = commandMap.get(action.toUpperCase());
+            UserCommandMap userCommandMap = UserCommandMap.getInstance();
+            command = userCommandMap.get(action.toUpperCase());
             return command;
         } catch (IllegalArgumentException e) {
-            request.setAttribute("wrongAction", action + MessageManager.getProperty("message.wrongaction"));
+            //TODO log
+            MessageManager messageManager = MessageManager.getInstance();
+            request.setAttribute(WRONG_ACTION, action + messageManager.getProperty(WRONG_ACTION_MESSAGE));
         }
         return null;
     }
