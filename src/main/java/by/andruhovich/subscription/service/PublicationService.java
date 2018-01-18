e40fe2ef1091b1577f484d59e6a50ba36cd91597
AnastasiaAndruhovich550503 <anastasia.andruhovich@gmail.com>
2018-01-18 22:39:02 +0300

    Create PublicationService class

---
+++ src/main/java/by/andruhovich/subscription/service/PublicationService.java
@@ -0,0 +1,13 @@
+package by.andruhovich.subscription.service;
+
+import by.andruhovich.subscription.entity.Publication;
+import by.andruhovich.subscription.exception.ServiceTechnicalException;
+
+import java.util.List;
+
+public class PublicationService {
+
+    public List<Publication> findPublications(String pageNumber) throws ServiceTechnicalException {
+        return null;
+    }
+}
