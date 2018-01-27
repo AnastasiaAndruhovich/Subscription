7991758ec0769aa52948275d43ebf9e80a6920f9
AnastasiaAndruhovich550503 <anastasia.andruhovich@gmail.com>
2018-01-27 22:03:20 +0300

    Modify ServletSecurityFilter

--- src/main/java/by/andruhovich/subscription/filter/ServletSecurityFilter.java
+++ src/main/java/by/andruhovich/subscription/filter/ServletSecurityFilter.java
@@ -1,5 +1,8 @@
 package by.andruhovich.subscription.filter;
 
+import by.andruhovich.subscription.command.publication.ShowPublicationCommand;
+import by.andruhovich.subscription.type.ClientType;
+
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
@@ -7,22 +10,30 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-@WebFilter(urlPatterns = { "/controller" }, servletNames = { "ControllerServlet" })
+@WebFilter(dispatcherTypes = {
+        DispatcherType.REQUEST,
+        DispatcherType.FORWARD,
+        DispatcherType.INCLUDE},
+        urlPatterns = { "/controller" },
+        servletNames = { "ControllerServlet" })
 public class ServletSecurityFilter implements Filter {
     public void init(FilterConfig fConfig) {
     }
 
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
             throws IOException, ServletException {
+        final String CLIENT_TYPE = "userType";
         HttpServletRequest req = (HttpServletRequest) request;
         HttpServletResponse resp = (HttpServletResponse) response;
         HttpSession session = req.getSession();
-        ClientType type = (ClientType) session.getAttribute("userType");
+        ClientType type = (ClientType) session.getAttribute(CLIENT_TYPE);
+
         if (type == null) {
             type = ClientType.GUEST;
-            session.setAttribute("userType", type);
-            RequestDispatcher dispatcher = request.getServletContext()
-                    .getRequestDispatcher("/jsp/user/publicationlist.jsp");
+            session.setAttribute(CLIENT_TYPE, type);
+            ShowPublicationCommand showPublicationCommand = new ShowPublicationCommand();
+            String page = showPublicationCommand.execute(req, resp);
+            RequestDispatcher dispatcher = request.getServletContext().getRequestDispatcher(page);
             dispatcher.forward(req, resp);
             return;
         }
