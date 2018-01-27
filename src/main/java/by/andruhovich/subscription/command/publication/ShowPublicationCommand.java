87a5d6526203c4d1b7beec3c13fbd5dbd8945205
AnastasiaAndruhovich550503 <anastasia.andruhovich@gmail.com>
2018-01-27 23:07:48 +0300

    Modify ShowCommandPublication

--- src/main/java/by/andruhovich/subscription/command/publication/ShowPublicationCommand.java
+++ src/main/java/by/andruhovich/subscription/command/publication/ShowPublicationCommand.java
@@ -2,43 +2,67 @@ package by.andruhovich.subscription.command.publication;
 
 import by.andruhovich.subscription.command.BaseCommand;
 import by.andruhovich.subscription.entity.Publication;
+import by.andruhovich.subscription.exception.MissingResourceTechnicalException;
 import by.andruhovich.subscription.exception.ServiceTechnicalException;
+import by.andruhovich.subscription.type.ClientType;
 import by.andruhovich.subscription.manager.ConfigurationManager;
 import by.andruhovich.subscription.manager.MessageManager;
 import by.andruhovich.subscription.service.PublicationService;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
 public class ShowPublicationCommand implements BaseCommand {
     private PublicationService publicationService = new PublicationService();
 
     private static final String PAGE_NUMBER = "pageNumber";
-    private static final String PUBLICATION_PAGE = "path.page.publication";
+    private static final String PAGE_COUNT = "pageCount";
+    private static final String PUBLICATION_USER_PAGE = "path.page.user.publicationList";
+    private static final String PUBLICATION_ADMIN_PAGE = "path.page.admin.publicationList";
     private static final String PUBLICATION_LIST_ATTRIBUTE = "publications";
-    private static final String PUBLICATION_MESSAGE_ATTRIBUTE = "publicationIsAbsent";
-    private static final String PUBLICATION_MESSAGE = "message.absent";
+    private static final String INFORMATION_MESSAGE_ATTRIBUTE = "informationIsAbsent";
+    private static final String PUBLICATION_MESSAGE = "message.informationIsAbsent";
     private static final String ERROR_PAGE = "path.page.error";
+    private static final String CLIENT_TYPE = "clientType";
 
     @Override
     public String execute(HttpServletRequest request, HttpServletResponse response) {
         String page;
-        String pageNumber = request.getParameter(PAGE_NUMBER);
         ConfigurationManager configurationManager = ConfigurationManager.getInstance();
         MessageManager messageManager = MessageManager.getInstance();
+
+        String pageNumber = request.getParameter(PAGE_NUMBER);
+        pageNumber = (pageNumber == null) ? "1" : pageNumber;
 
         try {
             List<Publication> publications = publicationService.showPublications(pageNumber);
             if (!publications.isEmpty()) {
+                int pageCount = publicationService.findPublicationPageCount();
                 request.setAttribute(PUBLICATION_LIST_ATTRIBUTE, publications);
+                request.setAttribute(PAGE_NUMBER, pageNumber);
+                request.setAttribute(PAGE_COUNT, pageCount);
             } else {
-                request.setAttribute(PUBLICATION_MESSAGE_ATTRIBUTE, "Publications" + messageManager.getProperty(PUBLICATION_MESSAGE));
+                request.setAttribute(INFORMATION_MESSAGE_ATTRIBUTE, messageManager.getProperty(PUBLICATION_MESSAGE));
             }
-            page = configurationManager.getProperty(PUBLICATION_PAGE);
-        } catch (ServiceTechnicalException e) {
+
+            HttpSession session = request.getSession();
+            ClientType type = (ClientType) session.getAttribute(CLIENT_TYPE);
+            if (type.equals(ClientType.ADMIN)) {
+                page = configurationManager.getProperty(PUBLICATION_ADMIN_PAGE);
+            }
+            else {
+                page = configurationManager.getProperty(PUBLICATION_USER_PAGE);
+            }
+        } catch (ServiceTechnicalException | MissingResourceTechnicalException e) {
             //log
-            page = configurationManager.getProperty(ERROR_PAGE);
+            try {
+                page = configurationManager.getProperty(ERROR_PAGE);
+            } catch (MissingResourceTechnicalException e1) {
+                //?????
+                page = null;
+            }
         }
         return page;
     }
