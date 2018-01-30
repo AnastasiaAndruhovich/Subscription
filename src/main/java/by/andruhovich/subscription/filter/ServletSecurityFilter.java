7b7d68f630532859981d8ea6036aace446ee4830
AnastasiaAndruhovich550503 <anastasia.andruhovich@gmail.com>
2018-01-30 10:25:35 +0300

    Add locale

--- src/main/java/by/andruhovich/subscription/filter/ServletSecurityFilter.java
+++ src/main/java/by/andruhovich/subscription/filter/ServletSecurityFilter.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.Locale;
 
 @WebFilter(dispatcherTypes = {
         DispatcherType.REQUEST,
@@ -23,6 +24,9 @@ public class ServletSecurityFilter implements Filter {
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
             throws IOException, ServletException {
         final String CLIENT_TYPE = "clientType";
+        final String LOCALE = "locale";
+        final String country = "US";
+        final String language = "EN";
         HttpServletRequest req = (HttpServletRequest) request;
         HttpServletResponse resp = (HttpServletResponse) response;
         HttpSession session = req.getSession();
@@ -30,7 +34,9 @@ public class ServletSecurityFilter implements Filter {
 
         if (type == null) {
             type = ClientType.GUEST;
+            Locale locale = new Locale(language, country);
             session.setAttribute(CLIENT_TYPE, type);
+            session.setAttribute(LOCALE, locale);
             ShowPublicationCommand showPublicationCommand = new ShowPublicationCommand();
             String page = showPublicationCommand.execute(req, resp);
             RequestDispatcher dispatcher = request.getServletContext().getRequestDispatcher(page);
