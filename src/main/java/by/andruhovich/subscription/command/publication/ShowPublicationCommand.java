4938013ff2b2dd3ad7a0ab534bdc4ce3f23c95cb
AnastasiaAndruhovich550503 <anastasia.andruhovich@gmail.com>
2018-02-03 10:44:28 +0300

    Refactor method

--- src/main/java/by/andruhovich/subscription/command/publication/ShowPublicationCommand.java
+++ src/main/java/by/andruhovich/subscription/command/publication/ShowPublicationCommand.java
@@ -1,24 +1,15 @@
 package by.andruhovich.subscription.command.publication;
 
 import by.andruhovich.subscription.command.BaseCommand;
-import by.andruhovich.subscription.entity.Publication;
-import by.andruhovich.subscription.exception.MissingResourceTechnicalException;
-import by.andruhovich.subscription.exception.ServiceTechnicalException;
-import by.andruhovich.subscription.type.ClientType;
-import by.andruhovich.subscription.manager.PageManager;
-import by.andruhovich.subscription.manager.LocaleManager;
-import by.andruhovich.subscription.service.PublicationService;
+import by.andruhovich.subscription.command.common.ShowEntityList;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-import java.util.List;
-import java.util.Locale;
 
 public class ShowPublicationCommand extends BaseCommand {
 
     @Override
     public String execute(HttpServletRequest request, HttpServletResponse response) {
-        return showPublicationList(request, response);
+        return ShowEntityList.showPublicationList(request, response);
     }
 }
