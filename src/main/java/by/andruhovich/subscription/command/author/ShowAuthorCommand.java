766466fa5d466d6e1ee0c66f5d0d06588b94d34c
AnastasiaAndruhovich550503 <anastasia.andruhovich@gmail.com>
2018-02-03 10:42:56 +0300

    Replace method bode for static method from ShowEntityList

--- src/main/java/by/andruhovich/subscription/command/author/ShowAuthorCommand.java
+++ src/main/java/by/andruhovich/subscription/command/author/ShowAuthorCommand.java
@@ -1,6 +1,7 @@
 package by.andruhovich.subscription.command.author;
 
 import by.andruhovich.subscription.command.BaseCommand;
+import by.andruhovich.subscription.command.common.ShowEntityList;
 import by.andruhovich.subscription.entity.Author;
 import by.andruhovich.subscription.exception.MissingResourceTechnicalException;
 import by.andruhovich.subscription.exception.ServiceTechnicalException;
@@ -16,57 +17,9 @@ import java.util.List;
 import java.util.Locale;
 
 public class ShowAuthorCommand extends BaseCommand {
-    private AuthorService authorService = new AuthorService();
-
-    private static final String PAGE_NUMBER = "pageNumber";
-    private static final String PAGE_COUNT = "pageCount";
-    private static final String PUBLICATION_USER_PAGE = "path.page.user.authorList";
-    private static final String PUBLICATION_ADMIN_PAGE = "path.page.admin.authorList";
-    private static final String PUBLICATION_LIST_ATTRIBUTE = "authors";
-    private static final String INFORMATION_MESSAGE_ATTRIBUTE = "informationIsAbsent";
-    private static final String PUBLICATION_MESSAGE = "message.informationIsAbsent";
-    private static final String ERROR_PAGE = "path.page.error";
-    private static final String CLIENT_TYPE = "clientType";
-    private static final String LOCALE = "locale";
-
     @Override
     public String execute(HttpServletRequest request, HttpServletResponse response) {
-        String page;
-        PageManager pageManager = PageManager.getInstance();
-        Locale locale = (Locale)request.getSession().getAttribute(LOCALE);
-        LocaleManager localeManager = new LocaleManager(locale);
-
-        String pageNumber = request.getParameter(PAGE_NUMBER);
-        pageNumber = (pageNumber == null) ? "1" : pageNumber;
-
-        try {
-            List<Author> authors = authorService.showAuthors(pageNumber);
-            if (!authors.isEmpty()) {
-                int pageCount = authorService.findAuthorPageCount();
-                request.setAttribute(PUBLICATION_LIST_ATTRIBUTE, authors);
-                request.setAttribute(PAGE_NUMBER, pageNumber);
-                request.setAttribute(PAGE_COUNT, pageCount);
-            } else {
-                request.setAttribute(INFORMATION_MESSAGE_ATTRIBUTE, localeManager.getProperty(PUBLICATION_MESSAGE));
-            }
-
-            HttpSession session = request.getSession();
-            ClientType type = (ClientType) session.getAttribute(CLIENT_TYPE);
-            if (type.equals(ClientType.ADMIN)) {
-                page = pageManager.getProperty(PUBLICATION_ADMIN_PAGE);
-            }
-            else {
-                page = pageManager.getProperty(PUBLICATION_USER_PAGE);
-            }
-        } catch (ServiceTechnicalException | MissingResourceTechnicalException e) {
-            //log
-            try {
-                page = pageManager.getProperty(ERROR_PAGE);
-            } catch (MissingResourceTechnicalException e1) {
-                //?????
-                page = null;
-            }
-        }
-        return page;
+        return ShowEntityList.showAuthorList(request, response);
     }
+
 }
