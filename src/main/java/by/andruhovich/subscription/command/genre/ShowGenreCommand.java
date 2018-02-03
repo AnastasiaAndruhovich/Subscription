d151216d10430e566c597db5f996b5a5b0fdcc87
AnastasiaAndruhovich550503 <anastasia.andruhovich@gmail.com>
2018-02-03 11:09:45 +0300

    Replace body method with static method from ShowEntityList

--- src/main/java/by/andruhovich/subscription/command/genre/ShowGenreCommand.java
+++ src/main/java/by/andruhovich/subscription/command/genre/ShowGenreCommand.java
@@ -1,6 +1,7 @@
 package by.andruhovich.subscription.command.genre;
 
 import by.andruhovich.subscription.command.BaseCommand;
+import by.andruhovich.subscription.command.common.ShowEntityList;
 import by.andruhovich.subscription.entity.Genre;
 import by.andruhovich.subscription.exception.MissingResourceTechnicalException;
 import by.andruhovich.subscription.exception.ServiceTechnicalException;
@@ -16,57 +17,8 @@ import java.util.List;
 import java.util.Locale;
 
 public class ShowGenreCommand extends BaseCommand {
-    private GenreService genreService = new GenreService();
-
-    private static final String PAGE_NUMBER = "pageNumber";
-    private static final String PAGE_COUNT = "pageCount";
-    private static final String GENRE_USER_PAGE = "path.page.user.genreList";
-    private static final String GENRE_ADMIN_PAGE = "path.page.admin.genreList";
-    private static final String PUBLICATION_LIST_ATTRIBUTE = "genres";
-    private static final String INFORMATION_MESSAGE_ATTRIBUTE = "informationIsAbsent";
-    private static final String PUBLICATION_MESSAGE = "message.informationIsAbsent";
-    private static final String ERROR_PAGE = "path.page.error";
-    private static final String CLIENT_TYPE = "clientType";
-    private static final String LOCALE = "locale";
-
     @Override
     public String execute(HttpServletRequest request, HttpServletResponse response) {
-        String page;
-        PageManager pageManager = PageManager.getInstance();
-        Locale locale = (Locale)request.getSession().getAttribute(LOCALE);
-        LocaleManager localeManager = new LocaleManager(locale);
-
-        String pageNumber = request.getParameter(PAGE_NUMBER);
-        pageNumber = (pageNumber == null) ? "1" : pageNumber;
-
-        try {
-            List<Genre> genres = genreService.showGenres(pageNumber);
-            if (!genres.isEmpty()) {
-                int pageCount = genreService.findGenrePageCount();
-                request.setAttribute(PUBLICATION_LIST_ATTRIBUTE, genres);
-                request.setAttribute(PAGE_NUMBER, pageNumber);
-                request.setAttribute(PAGE_COUNT, pageCount);
-            } else {
-                request.setAttribute(INFORMATION_MESSAGE_ATTRIBUTE, localeManager.getProperty(PUBLICATION_MESSAGE));
-            }
-
-            HttpSession session = request.getSession();
-            ClientType type = (ClientType) session.getAttribute(CLIENT_TYPE);
-            if (type.equals(ClientType.ADMIN)) {
-                page = pageManager.getProperty(GENRE_ADMIN_PAGE);
-            }
-            else {
-                page = pageManager.getProperty(GENRE_USER_PAGE);
-            }
-        } catch (ServiceTechnicalException | MissingResourceTechnicalException e) {
-            //log
-            try {
-                page = pageManager.getProperty(ERROR_PAGE);
-            } catch (MissingResourceTechnicalException e1) {
-                //?????
-                page = null;
-            }
-        }
-        return page;
+        return ShowEntityList.showGenreList(request, response);
     }
 }
