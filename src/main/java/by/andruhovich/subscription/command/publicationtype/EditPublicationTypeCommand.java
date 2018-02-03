87c0ed3a82abae61786e4002992b865d1506ae43
AnastasiaAndruhovich550503 <anastasia.andruhovich@gmail.com>
2018-02-03 21:30:18 +0300

    Create EditPublicationTypeCommand

---
+++ src/main/java/by/andruhovich/subscription/command/publicationtype/EditPublicationTypeCommand.java
@@ -0,0 +1,65 @@
+package by.andruhovich.subscription.command.publicationtype;
+
+import by.andruhovich.subscription.command.BaseCommand;
+import by.andruhovich.subscription.entity.PublicationType;
+import by.andruhovich.subscription.exception.MissingResourceTechnicalException;
+import by.andruhovich.subscription.exception.ServiceTechnicalException;
+import by.andruhovich.subscription.manager.LocaleManager;
+import by.andruhovich.subscription.manager.PageManager;
+import by.andruhovich.subscription.service.PublicationTypeService;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Locale;
+
+public class EditPublicationTypeCommand extends BaseCommand {
+    private PublicationTypeService publicationTypeService = new PublicationTypeService();
+
+    private static final String EDIT_PUBLICATION_TYPE_ADMIN_PAGE = "path.page.admin.editPublicationType";
+
+    private static final String PUBLICATION_TYPE_ID_ATTRIBUTE = "publicationTypeId";
+    private static final String NAME_ATTRIBUTE = "name";
+    private static final String RESULT_EDIT_PUBLICATION_TYPE_ATTRIBUTE = "result";
+    private static final String PUBLICATION_TYPE_ATTRIBUTE = "publicationType";
+
+    private static final String SUCCESSFUL_EDIT_PUBLICATION_TYPE_MESSAGE = "message.successfulEditPublicationType";
+    private static final String ERROR_EDIT_PUBLICATION_TYPE_MESSAGE = "message.errorEditPublicationType";
+
+    private static final Logger LOGGER = LogManager.getLogger(EditPublicationTypeCommand.class);
+
+    @Override
+    public String execute(HttpServletRequest request, HttpServletResponse response) {
+        String page;
+        PageManager pageManager = PageManager.getInstance();
+        Locale locale = (Locale) request.getSession().getAttribute(LOCALE);
+        LocaleManager localeManager = new LocaleManager(locale);
+
+        String publicationTypeId = request.getParameter(PUBLICATION_TYPE_ID_ATTRIBUTE);
+        String name = request.getParameter(NAME_ATTRIBUTE);
+
+        int id = Integer.parseInt(publicationTypeId);
+        PublicationType publicationType = new PublicationType(id, name);
+        request.setAttribute(PUBLICATION_TYPE_ATTRIBUTE, publicationType);
+
+        try {
+            if (publicationTypeService.updatePublicationType(publicationTypeId, name)) {
+                String successfulEditPublicationTypeMessage = localeManager.getProperty(SUCCESSFUL_EDIT_PUBLICATION_TYPE_MESSAGE);
+                request.setAttribute(RESULT_EDIT_PUBLICATION_TYPE_ATTRIBUTE, successfulEditPublicationTypeMessage);
+            } else {
+                String errorEditPublicationTypeMessage = localeManager.getProperty(ERROR_EDIT_PUBLICATION_TYPE_MESSAGE);
+                request.setAttribute(RESULT_EDIT_PUBLICATION_TYPE_ATTRIBUTE, errorEditPublicationTypeMessage);
+            }
+            page = pageManager.getProperty(EDIT_PUBLICATION_TYPE_ADMIN_PAGE);
+        } catch (ServiceTechnicalException e) {
+            LOGGER.log(Level.ERROR, "Database error connection");
+            page = ERROR_PAGE;
+        } catch (MissingResourceTechnicalException e) {
+            LOGGER.log(Level.ERROR, e.getMessage());
+            page = ERROR_PAGE;
+        }
+        return page;
+    }
+}
---
+++ src/main/java/by/andruhovich/subscription/command/publicationtype/ParsePublicationTypeCommand.java
@@ -0,0 +1,43 @@
+package by.andruhovich.subscription.command.publicationtype;
+
+import by.andruhovich.subscription.command.BaseCommand;
+import by.andruhovich.subscription.entity.PublicationType;
+import by.andruhovich.subscription.exception.MissingResourceTechnicalException;
+import by.andruhovich.subscription.manager.PageManager;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ParsePublicationTypeCommand extends BaseCommand {
+    private static final String EDIT_PUBLICATION_TYPE_ADMIN_PAGE = "path.page.admin.editPublicationType";
+
+    private static final String PUBLICATION_TYPE_ID_ATTRIBUTE = "publicationTypeId";
+    private static final String NAME_ATTRIBUTE = "name";
+    private static final String PUBLICATION_TYPE_ATTRIBUTE = "publicationType";
+
+    private static final Logger LOGGER = LogManager.getLogger(EditPublicationTypeCommand.class);
+
+    @Override
+    public String execute(HttpServletRequest request, HttpServletResponse response) {
+        String page;
+        PageManager pageManager = PageManager.getInstance();
+
+        String genreId = request.getParameter(PUBLICATION_TYPE_ID_ATTRIBUTE);
+        String name = request.getParameter(NAME_ATTRIBUTE);
+
+        int id = Integer.parseInt(genreId);
+        PublicationType publicationType = new PublicationType(id, name);
+        request.setAttribute(PUBLICATION_TYPE_ATTRIBUTE, publicationType);
+
+        try {
+            page = pageManager.getProperty(EDIT_PUBLICATION_TYPE_ADMIN_PAGE);
+        } catch (MissingResourceTechnicalException e) {
+            LOGGER.log(Level.ERROR, e.getMessage());
+            page = ERROR_PAGE;
+        }
+        return page;
+    }
+}
