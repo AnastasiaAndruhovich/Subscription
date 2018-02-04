0330199ea8d3974a03508bdb7e9d24b17a07f235
AnastasiaAndruhovich550503 <anastasia.andruhovich@gmail.com>
2018-02-04 23:52:59 +0300

    Delete InformationIsAbsentMessage

--- src/main/java/by/andruhovich/subscription/command/BaseCommand.java
+++ src/main/java/by/andruhovich/subscription/command/BaseCommand.java
@@ -1,27 +1,10 @@
 package by.andruhovich.subscription.command;
 
-import by.andruhovich.subscription.entity.Publication;
-import by.andruhovich.subscription.exception.MissingResourceTechnicalException;
-import by.andruhovich.subscription.exception.ServiceTechnicalException;
-import by.andruhovich.subscription.manager.LocaleManager;
-import by.andruhovich.subscription.manager.PageManager;
-import by.andruhovich.subscription.service.PublicationService;
-import by.andruhovich.subscription.type.ClientType;
-import org.apache.logging.log4j.Level;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-import java.util.List;
-import java.util.Locale;
 
 public abstract class BaseCommand {
     protected static final String ERROR_PAGE = "/jsp/error/error.jsp";
-
-    protected static final String INFORMATION_IS_ABSENT_ATTRIBUTE = "informationIsAbsent";
-    protected static final String INFORMATION_IS_ABSENT_MESSAGE = "message.informationIsAbsent";
 
     protected static final String CLIENT_TYPE = "clientType";
     protected static final String CLIENT_ID = "clientId";
