df79be5bcd1a2959220c9ff528b3b96598c9dffe
AnastasiaAndruhovich550503 <anastasia.andruhovich@gmail.com>
2018-02-05 17:17:37 +0300

    Add body in UnblockUserCommand method

--- src/main/java/by/andruhovich/subscription/command/user/UnblockUserCommand.java
+++ src/main/java/by/andruhovich/subscription/command/user/UnblockUserCommand.java
@@ -1,13 +1,52 @@
 package by.andruhovich.subscription.command.user;
 
 import by.andruhovich.subscription.command.BaseCommand;
+import by.andruhovich.subscription.command.common.ShowEntityList;
+import by.andruhovich.subscription.exception.MissingResourceTechnicalException;
+import by.andruhovich.subscription.exception.ServiceTechnicalException;
+import by.andruhovich.subscription.manager.LocaleManager;
+import by.andruhovich.subscription.service.UserService;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Locale;
 
 public class UnblockUserCommand extends BaseCommand {
+    private UserService userService = new UserService();
+
+    private static final String CLIENT_ID_ATTRIBUTE = "clientId";
+    private static final String USER_ID_ATTRIBUTE = "userId";
+    private static final String RESULT_UNBLOCK_USER_ATTRIBUTE = "result";
+
+    private static final String ERROR_UNBLOCK_MESSAGE = "message.errorUnblockUser";
+
+    private static final Logger LOGGER = LogManager.getLogger(UnblockUserCommand.class);
+
     @Override
     public String execute(HttpServletRequest request, HttpServletResponse response) {
-        return null;
+        String page;
+        Locale locale = (Locale) request.getSession().getAttribute(LOCALE);
+        LocaleManager localeManager = new LocaleManager(locale);
+
+        String userId = request.getParameter(USER_ID_ATTRIBUTE);
+        Integer adminId = (Integer) request.getSession().getAttribute(CLIENT_ID_ATTRIBUTE);
+
+        try {
+            if (!userService.unblockUser(userId, adminId.toString())) {
+                String errorUnblockUserMessage = localeManager.getProperty(ERROR_UNBLOCK_MESSAGE);
+                request.setAttribute(RESULT_UNBLOCK_USER_ATTRIBUTE, errorUnblockUserMessage);
+            }
+            return ShowEntityList.showUserList(request, response);
+        } catch (ServiceTechnicalException e) {
+            LOGGER.log(Level.ERROR, "Database error connection");
+            page = ERROR_PAGE;
+        } catch (MissingResourceTechnicalException e) {
+            LOGGER.log(Level.ERROR, e.getMessage());
+            page = ERROR_PAGE;
+        }
+        return page;
     }
 }
