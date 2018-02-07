fbcb05b508875e21f279898eddb8a37a7f3378ee
AnastasiaAndruhovich550503 <anastasia.andruhovich@gmail.com>
2018-02-07 09:47:34 +0300

    Refactor return page

--- src/main/java/by/andruhovich/subscription/command/payment/AddPaymentCommand.java
+++ src/main/java/by/andruhovich/subscription/command/payment/AddPaymentCommand.java
@@ -1,13 +1,14 @@
 package by.andruhovich.subscription.command.payment;
 
 import by.andruhovich.subscription.command.BaseCommand;
-import by.andruhovich.subscription.command.common.ShowEntityList;
 import by.andruhovich.subscription.entity.Payment;
+import by.andruhovich.subscription.entity.Subscription;
 import by.andruhovich.subscription.exception.MissingResourceTechnicalException;
 import by.andruhovich.subscription.exception.ServiceTechnicalException;
 import by.andruhovich.subscription.manager.LocaleManager;
 import by.andruhovich.subscription.manager.PageManager;
 import by.andruhovich.subscription.service.PaymentService;
+import by.andruhovich.subscription.service.SubscriptionService;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -18,10 +19,15 @@ import java.util.Locale;
 
 public class AddPaymentCommand extends BaseCommand {
     private PaymentService paymentService = new PaymentService();
+    private SubscriptionService subscriptionService = new SubscriptionService();
+
+    private static final String SUBSCRIBE_PAGE = "path.page.user.paySubscription";
 
     private static final String SUBSCRIPTION_ID_ATTRIBUTE = "subscriptionId";
-    private static final String RESULT_ADD_GENRE_ATTRIBUTE = "result";
+    private static final String SUBSCRIPTION_ATTRIBUTE = "subscription";
+    private static final String RESULT_PAY_SUBSCRIPTION_ATTRIBUTE = "result";
 
+    private static final String SUCCESSFUL_PAY_SUBSCRIPTION_MESSAGE = "message.successfulPaySubscription";
     private static final String ERROR_PAY_SUBSCRIPTION_MESSAGE = "message.errorPaySubscription";
 
     private static final Logger LOGGER = LogManager.getLogger(AddPaymentCommand.class);
@@ -29,6 +35,7 @@ public class AddPaymentCommand extends BaseCommand {
     @Override
     public String execute(HttpServletRequest request, HttpServletResponse response) {
         String page;
+        PageManager pageManager = PageManager.getInstance();
         Locale locale = (Locale) request.getSession().getAttribute(LOCALE);
         LocaleManager localeManager = new LocaleManager(locale);
 
@@ -37,11 +44,17 @@ public class AddPaymentCommand extends BaseCommand {
 
         try {
             Payment payment = paymentService.addPayment(clientId.toString(), subscriptionId);
-            if (!payment.isStatement()) {
-                String errorAddedPublicationMessage = localeManager.getProperty(ERROR_PAY_SUBSCRIPTION_MESSAGE);
-                request.setAttribute(RESULT_ADD_GENRE_ATTRIBUTE, errorAddedPublicationMessage);
+            if (payment.isStatement()) {
+                String successfulPaySubscriptionMessage = localeManager.getProperty(SUCCESSFUL_PAY_SUBSCRIPTION_MESSAGE);
+                request.setAttribute(RESULT_PAY_SUBSCRIPTION_ATTRIBUTE, successfulPaySubscriptionMessage);
             }
-            return ShowEntityList.findSubscriptionByUser(request, response);
+            else {
+                String errorPaySubscriptionMessage = localeManager.getProperty(ERROR_PAY_SUBSCRIPTION_MESSAGE);
+                request.setAttribute(RESULT_PAY_SUBSCRIPTION_ATTRIBUTE, errorPaySubscriptionMessage);
+            }
+            Subscription subscription = subscriptionService.findSubscriptionById(subscriptionId);
+            request.setAttribute(SUBSCRIPTION_ATTRIBUTE, subscription);
+            return pageManager.getProperty(SUBSCRIBE_PAGE);
         } catch (ServiceTechnicalException e) {
             LOGGER.log(Level.ERROR, "Database error connection");
             page = ERROR_PAGE;
