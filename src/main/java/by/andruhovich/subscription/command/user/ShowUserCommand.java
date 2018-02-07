d548de6be24291f5d50938698b4448da2e541bff
AnastasiaAndruhovich550503 <anastasia.andruhovich@gmail.com>
2018-02-07 16:18:28 +0300

    Delete unused import

--- src/main/java/by/andruhovich/subscription/command/user/ShowUserCommand.java
+++ src/main/java/by/andruhovich/subscription/command/user/ShowUserCommand.java
@@ -2,18 +2,9 @@ package by.andruhovich.subscription.command.user;
 
 import by.andruhovich.subscription.command.BaseCommand;
 import by.andruhovich.subscription.command.common.ShowEntityList;
-import by.andruhovich.subscription.entity.User;
-import by.andruhovich.subscription.exception.MissingResourceTechnicalException;
-import by.andruhovich.subscription.exception.ServiceTechnicalException;
-import by.andruhovich.subscription.manager.PageManager;
-import by.andruhovich.subscription.service.UserService;
-import org.apache.logging.log4j.Level;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.List;
 
 public class ShowUserCommand extends BaseCommand {
     @Override
