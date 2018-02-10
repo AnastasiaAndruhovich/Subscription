699ef8819b5a724231fa40d2fd76ade85a736b82
AnastasiaAndruhovich550503 <anastasia.andruhovich@gmail.com>
2018-02-10 14:53:52 +0300

    Refactor confirm password

--- web/js/confirmPassword.js
+++ web/js/confirmPassword.js
@@ -1,13 +1,9 @@
-var password = document.getElementById("password"),
-    confirm_password = document.getElementById("confirmPassword");
-
-function validatePassword(){
-    if(password.value != confirm_password.value) {
-        confirm_password.setCustomValidity("Passwords Don't Match");
+var checkPassword = function() {
+    if (document.getElementById('password').value === document.getElementById('confirmPassword').value) {
+        document.getElementById('message').style.color = 'green';
+        document.getElementById('message').innerHTML = 'matching';
     } else {
-        confirm_password.setCustomValidity('');
+        document.getElementById('message').style.color = 'red';
+        document.getElementById('message').innerHTML = 'not matching';
     }
 }
-
-password.onchange = validatePassword;
-confirm_password.onkeyup = validatePassword;
